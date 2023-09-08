08a4c0dfc620c3146506d94695f269d459f42b3d
Daniel <danielspradah0@gmail.com>
2023-09-08 12:41:31 -0500

    First commit

---
+++ src/primertallerestructdatos/Persona.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primertallerestructdatos;
+
+/**
+ *
+ * @author danie
+ */
+public class Persona {
+    
+    protected String nombre;
+    protected String apellido;
+    protected byte edad;
+
+    public Persona(String nombre, String apellido, byte edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public byte getEdad() {
+        return edad;
+    }
+
+    public void setEdad(byte edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+    
+    
+}
