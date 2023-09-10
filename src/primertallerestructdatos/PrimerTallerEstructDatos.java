dbcc53b066de7a024efff58215944dd820070799
Daniel <danielspradah0@gmail.com>
2023-09-10 12:16:45 -0500

    Add ascending vector sorting and demo in main class
    Used insertion sort learned reading, has time complexity of O(n) best case, and O(n^2) worst case.
    Pending: Asking professor wether efficiency will be marked here.

--- src/primertallerestructdatos/PrimerTallerEstructDatos.java
+++ src/primertallerestructdatos/PrimerTallerEstructDatos.java
@@ -4,6 +4,8 @@
  */
 package primertallerestructdatos;
 
+import java.util.Arrays;
+
 /**
  *
  * @author danie
@@ -14,11 +16,41 @@ public class PrimerTallerEstructDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            Estudiante e = new Estudiante("Daniel", "Prada", (byte)18, "systems engineering", (byte)5);
-            e.saludar();
-            e.setEdad((byte)18);
-            System.out.println(e.getEdad());
-            System.out.println(e.getApellido());
+            Estudiante est = new Estudiante("Daniel", "Prada", (byte)18, "systems engineering", (byte)5);
+            Profesor prof = new Profesor("Hoover", "Rueda", (byte)30, 6000000);
+            est.saludar();
+            est.setEdad((byte)18);
+            System.out.println(est.getEdad());
+            System.out.println(est.getApellido());
+            
+            prof.saludar();
+            
+            int[] li = {1,2,4,5,3};
+            System.out.println(Arrays.toString(ordenarNumeros(li, "ascendente")));
+            
+    }
+    
+    public static int[] ordenarNumeros(int[] vector, String tipoOrden) {
+        
+//        Aprendí insertion sort leyendo Introduction to Algorithms Fourth Edition de Thomas H. Cormen, Charles E. Leiserson,
+//        Ronald L. Rivest, Clifford Stein
+        if (tipoOrden.equals("ascendente")) {
+            
+            for (int i = 1; i < vector.length; i++) {
+                int picked = vector[i];
+                int prev = i-1;
+                
+                while ( (prev >= 0) && (picked < vector[prev]) ) {
+                        vector[prev+1] = vector[prev];
+                        prev--;
+                }
+                
+                vector[prev+1] = picked;
+            }
+            
+        }
+        
+        return vector;
     }
     
 }
