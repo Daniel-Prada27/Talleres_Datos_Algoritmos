445b45783dcfca7415f5c72cbda225ec57c64221
Daniel <danielspradah0@gmail.com>
2023-09-10 12:59:41 -0500

    Add String Vector sorting and demo in main class

--- src/primertallerestructdatos/PrimerTallerEstructDatos.java
+++ src/primertallerestructdatos/PrimerTallerEstructDatos.java
@@ -16,59 +16,110 @@ public class PrimerTallerEstructDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            Estudiante est = new Estudiante("Daniel", "Prada", (byte)18, "systems engineering", (byte)5);
-            Profesor prof = new Profesor("Hoover", "Rueda", (byte)30, 6000000);
-            est.saludar();
-            est.setEdad((byte)18);
-            System.out.println(est.getEdad());
-            System.out.println(est.getApellido());
-            
-            prof.saludar();
-            
-            int[] list = {1,4,2,5,3};
-            System.out.println(Arrays.toString(ordenarNumeros(list, "ascendente")));
-            
-            // Lo desordenamos de nuevo;
-            int[] secondList = {1,2,4,5,3};
-            System.out.println(Arrays.toString(ordenarNumeros(secondList, "descendente")));
-            
+        Estudiante est = new Estudiante("Daniel", "Prada", (byte) 18, "systems engineering", (byte) 5);
+        Profesor prof = new Profesor("Hoover", "Rueda", (byte) 30, 6000000);
+        est.saludar();
+        est.setEdad((byte) 18);
+        System.out.println(est.getEdad());
+        System.out.println(est.getApellido());
+
+        prof.saludar();
+
+        int[] list = {1, 4, 2, 5, 3};
+        System.out.println(Arrays.toString(ordenarNumeros(list, "ascendente")));
+
+        // Lo desordenamos de nuevo;
+        int[] secondList = {1, 2, 4, 5, 3};
+        System.out.println(Arrays.toString(ordenarNumeros(secondList, "descendente")));
+
+        String[] strings = {"aa", "b", "abcc", "ccc"};
+        System.out.println(Arrays.toString(ordenarStrings(strings, "ascendente")));
+
+        String[] segundoStrings = {"aa", "b", "abcc", "ccc"};
+        System.out.println(Arrays.toString(ordenarStrings(segundoStrings, "descendente")));
+
     }
-    
+
     public static int[] ordenarNumeros(int[] vector, String tipoOrden) {
-        
+
 //        Aprendí insertion sort leyendo Introduction to Algorithms Fourth Edition de Thomas H. Cormen, Charles E. Leiserson,
 //        Ronald L. Rivest, Clifford Stein
         switch (tipoOrden) {
             case "ascendente" -> {
                 for (int i = 1; i < vector.length; i++) {
                     int picked = vector[i];
-                    int prev = i-1;
-                    
-                    while ( (prev >= 0) && (picked < vector[prev]) ) {
-                        vector[prev+1] = vector[prev];
+                    int prev = i - 1;
+
+                    while ((prev >= 0) && (picked < vector[prev])) {
+                        vector[prev + 1] = vector[prev];
                         prev--;
                     }
-                    
-                    vector[prev+1] = picked;
+
+                    vector[prev + 1] = picked;
                 }
             }
             case "descendente" -> {
                 for (int i = 1; i < vector.length; i++) {
                     int picked = vector[i];
-                    int prev = i-1;
+                    int prev = i - 1;
                     // El cambio está en picked > vector[prev]
-                    while ( (prev >= 0) && (picked > vector[prev]) ) {
-                        vector[prev+1] = vector[prev];
+                    while ((prev >= 0) && (picked > vector[prev])) {
+                        vector[prev + 1] = vector[prev];
                         prev--;
                     }
-                    
-                    vector[prev+1] = picked;
+
+                    vector[prev + 1] = picked;
                 }
             }
-            default -> System.out.println("Especifiacion de ordenamiento no valida, el vector no sera alterado");
+            default ->
+                System.out.println("Especifiacion de ordenamiento no valida, el vector no sera alterado");
         }
-        
+
         return vector;
     }
+
     
+    //Misma lógica que ordenarNumeros, pero con distinto tipo de dato
+    public static String[] ordenarStrings(String[] vector, String tipoOrden) {
+
+        switch (tipoOrden) {
+
+            case "ascendente" -> {
+
+                for (int i = 1; i < vector.length; i++) {
+                    String picked = vector[i];
+                    int prev = i - 1;
+
+                    while ((prev >= 0) && (picked.length() < vector[prev].length())) {
+                        vector[prev + 1] = vector[prev];
+                        prev--;
+                    }
+
+                    vector[prev + 1] = picked;
+                }
+
+            }
+
+            case "descendente" -> {
+
+                for (int i = 1; i < vector.length; i++) {
+                    String picked = vector[i];
+                    int prev = i - 1;
+                    // El cambio está en picked > vector[prev]
+                    while ((prev >= 0) && (picked.length() > vector[prev].length())) {
+                        vector[prev + 1] = vector[prev];
+                        prev--;
+                    }
+
+                    vector[prev + 1] = picked;
+                }
+            }
+            default ->
+                System.out.println("Especifiacion de ordenamiento no valida, el vector no sera alterado");
+        }
+
+        return vector;
+
+    }
+
 }
